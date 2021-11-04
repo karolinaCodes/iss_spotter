84c846e1b18e29b0be3522ec63e4933df33f88ba
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-11-03 21:42:00 -0400

    add refactored index2 and iss_promised- refactored using promises

---
+++ iss_promised.js
@@ -0,0 +1,35 @@
+const request = require("request-promise-native");
+
+const fetchMyIP = () => {
+  return request("https://api.ipify.org?format=json");
+};
+
+const fetchCoordsByIP = function (body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function (body) {
+  const {longitude, latitude} = JSON.parse(body);
+
+  return request(
+    `https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`
+  );
+};
+
+const nextISSTimesForMyLocation = function () {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then(data => {
+      const {response} = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation,
+};
